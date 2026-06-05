b084211cc0744001fc1556c9296ad0fd336a72f5
Aldo Mendoza <aldo.mendoza@emiworld.org>
2026-06-05 13:19:51 -0700

    Fix Service Worker absolute paths for GitHub Pages subfolder compatibility

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = 'mtg-companion-cache-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/favicon.svg',
-  '/icon-192.png',
-  '/icon-512.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './favicon.svg',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', (e) => {
